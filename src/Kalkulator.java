2f76014dc582a878cbe260890addff00cfcc9c58
markobutina <mbutina@gmail.com>
2023-04-16 14:05:08 +0200

    updates

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -2,30 +2,22 @@ public class Kalkulator {
     double operand1, operand2;
 
     double add(double operand1,double operand2){
-        this.operand1=operand1;
-        this.operand2=operand2;
-        double zbir = this.operand1 + this.operand2;
+        double zbir = operand1 + operand2;
         return zbir;
     }
 
     double sub(double operand1,double operand2){
-        this.operand1=operand1;
-        this.operand2=operand2;
-        double razlika = this.operand1 - this.operand2;
+        double razlika = operand1 - operand2;
         return razlika;
     }
 
     double mul(double operand1,double operand2){
-        this.operand1=operand1;
-        this.operand2=operand2;
-        double proizvod = this.operand1 * this.operand2;
+        double proizvod = operand1 * operand2;
         return proizvod;
     }
 
     double div(double operand1,double operand2){
-        this.operand1=operand1;
-        this.operand2=operand2;
-        double kolicnik = this.operand1 / this.operand2;
+        double kolicnik = operand1 / operand2;
         return kolicnik;
     }
 }
